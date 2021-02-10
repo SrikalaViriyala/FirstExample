36dc78a98e108af72b67377da8f97fb3c42ace5e
SrikalaViriyala <Srikala_viriyala@hotmail.com>
2021-02-10 07:03:39 -0500

    Commit to local

---
+++ FirstExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World new line!");
+        }
+    }
+}
