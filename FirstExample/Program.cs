49ff9e94f39d64198a349ed6fd2f4929e368baa1
SrikalaViriyala <78598605+SrikalaViriyala@users.noreply.github.com>
2021-02-10 07:14:52 -0500

    Fetch check

--- FirstExample/Program.cs
+++ FirstExample/Program.cs
@@ -9,6 +9,7 @@ namespace FirstExample
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World new line!");
             Console.WriteLine("Sync Code");
+            Console.WriteLine("Fetch Code");
         }
     }
 }
